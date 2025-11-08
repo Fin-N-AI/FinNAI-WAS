88f97cb64f5b1f9ac56c7269cfd5a6aa371dedb3
KangJiSeok <rkdwltjr5876@naver.com>
2025-11-08 16:27:15 +0900

    chore: initialize Spring Boot project setup

---
+++ src/test/java/com/finnai/FinNaiWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finnai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinNaiWasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
